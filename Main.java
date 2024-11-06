8c864718a15f35a538394a42cb21271e233735e3
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-11-06 11:03:36 -0800

    worked on 4.1

--- Main.java
+++ Main.java
@@ -4,6 +4,18 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+  }
+
+
+  public static void remove3rdLetter(String input) {
+    int index = 0;
+    while (index < input.length()) {
+      String letter = input.substring(index, index+1);
+      if (index + 1 % 3 != 0) {
+         System.out.print(letter);
+      }
+    }
+    System.out.print("/n");
   }
 }
